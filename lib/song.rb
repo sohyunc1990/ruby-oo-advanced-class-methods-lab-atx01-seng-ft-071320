5dbd458b5440dec6a774d6fe8f881510172aa1b1
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-19 04:32:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,9 @@ class Song
     song
   end
   
-  def self.new_by_name(string)
+  def self.new_by_name(songname)
     song = self.new
-    song.name = string
+    song.name = songname
     song
   end
  
