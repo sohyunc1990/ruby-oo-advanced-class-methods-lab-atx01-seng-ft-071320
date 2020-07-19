e1abeb8b0a131ec7887b35ff4e45038bde5cf30c
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-19 04:16:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,25 +2,13 @@ require'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-  
-  def self.create
-    song = self.new
-    song.save
-    song
-  end 
-  
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-  
-  def self.new_by_name
-  song = self.new_by_name(song_name)
-  song.name
-  end
-
-
+ 
 end
